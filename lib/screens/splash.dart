28cba067b0ae4ff13b08c376634fe47036288dc0
Silent Coder <premsingh@tuta.io>
2025-11-18 21:45:00 +0530

    Update splash.dart

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, '/main');
+      Navigator.pushReplacementNamed(context, '/home');
     });
   }
 
